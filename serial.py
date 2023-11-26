ee9e88a618eacb0e6610228331612c84ba74b9b9
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-11-26 16:25:07 -0600

    update utils

--- serial.py
+++ serial.py
@@ -1,2 +1,23 @@
-def ingest(table_data):
-     
+import util
+
+class State:
+    def __init__(self):
+        self.row = -1
+        self.col = -1
+    
+    def update(row, col):
+        self.row = row
+        self.col = col
+
+class TableSerializer:
+    def __init__(self):
+        self.tokenizer = transformers.BertTokenizerFast.from_pretrained('bert-base-uncased')
+        self.progress_state = State() 
+        
+    def serialize(table_data):
+        util.preprocess_schema(self.tokenizer, table_data)
+        while (True):
+
+
+            break
+
--- util.py
+++ util.py
@@ -1,10 +1,9 @@
 import transformers
 import src.model
 
-
-def get_tokenizer():
-    tokenizer = transformers.BertTokenizerFast.from_pretrained('bert-base-uncased')
-    return tokenizer
+Max_Title_Size = 60
+Max_Col_Header_Size = 30
+Max_Cell_Size = 100
 
 def get_encode_model(model_path, device):
     model = src.model.Retriever.from_pretrained(model_path) 
@@ -13,3 +12,27 @@ def get_encode_model(model_path, device):
     model = model.half() #fp16
     return model
 
+def truncate(tokenizer, text, max_size):
+    tokens = tokenizer.tokenize(text)
+    if len(tokens > max_size):
+        updated_tokens = tokens[:max_size]
+        updated_text = tokenize.decode(tokenizer.convert_tokens_to_ids(updated_tokens))
+    else:
+        updated_text = text
+    return updated_text
+
+def preprocess_schema(tokenizer, table_data):
+    title = table_data['document_title']
+    table_data['document_title'] = truncate(tokenizer, title, Max_Title_Size) 
+
+    col_data = table_data['columns']
+    for col_info in cell_data:
+        text = col_info['text']
+        col_info['text'] = truncate(tokenizer, text, Max_Col_Header_Size)
+
+def preprocess_row(row_item):
+    cell_lst = row_item['cells']
+    for cell_info in cell_lst:
+        text = cell_info['text']
+        cell_info['text'] = truncate(tokenizer, text, Max_Cell_Size)
+    
