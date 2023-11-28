a7899a3c52f3d63bf5c85ba850cd44ac01272a09
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-11-28 12:40:09 -0600

    serial parallel

--- test.py
+++ test.py
@@ -2,16 +2,29 @@ import json
 from tqdm import tqdm
 import os
 from serial import TableSerializer
+from multiprocessing import Pool as ProcessPool
 
 def read_tables():
     data_file = '/home/cc/code/solo_work/data/nyc_open_1000/tables/tables.jsonl'
+    table_lst = []
     with open(data_file) as f:
-        for line in f:
+        for line in tqdm(f):
             table_data = json.loads(line)
-            yield table_data
+            table_lst.append(table_data)
+    return table_lst
+
+def init_worker():
+    global tsl
+    tsl = TableSerializer()
+
+def process_table(arg_info):
+    table_data = arg_info['table_data']
+    block_lst = []
+    for serial_block in tsl.serialize(table_data):
+        block_lst.append(serial_block)
+    return block_lst
 
 def main():
-    tsl = TableSerializer()
     block_id = 0
     out_file = './output/nyc_1000_passages.jsonl'
     if os.path.exists(out_file):
@@ -20,8 +33,19 @@ def main():
             return
     f_o = open(out_file, 'w')
 
-    for table_data in tqdm(read_tables()):
-        for serial_block in tsl.serialize(table_data):
+    print('Loading tables')
+    table_lst = read_tables()
+    arg_info_lst = []
+    for table_data in table_lst:
+        arg_info = {
+            'table_data':table_data,
+        }
+        arg_info_lst.append(arg_info)
+
+    work_pool = ProcessPool(initializer=init_worker)
+    
+    for table_block_lst in tqdm(work_pool.imap_unordered(process_table, arg_info_lst), total=len(table_lst)):
+        for serial_block in table_block_lst:
             serial_block['p_id'] = block_id
             block_id += 1
             f_o.write(json.dumps(serial_block) + '\n')
