1fa31330982e479a773f4a3663ff4f66e4a4ac64
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-12-20 18:25:57 -0600

    log state

--- question/generate.py
+++ question/generate.py
@@ -21,6 +21,17 @@ def read_tables(args):
                 continue
             yield table_data
 
+def read_state(state_file):
+    if not os.path.isfile(state_file):
+        return None
+    with open(state_file) as f:
+        state = json.load(f)
+    return state
+
+def write_state(state_file, state):
+    with open(state_file, 'w') as f_o:
+        f_o.write(json.dumps(state))
+
 def main():
     args = get_args()
     out_dir = f'./output/{args.dataset}'
@@ -33,14 +44,27 @@ def main():
     table_iterator = read_tables(args)
     generator = ChatGptGenerator('./prompt')
     table_seq_no = 0
+   
+    state_file = os.path.join(out_dir, 'state.json')
+    state = read_state(state_file)
+    if state is None:
+        state = {'seq_no':None, 'id':None}
+
+    start_seq_no = 1
+    if state['seq_no'] is not None:
+        start_seq_no = state['seq_no']
+    
     for table_sql_lst in generator.generate_questions(table_iterator):
         table_seq_no += 1
         if len(table_sql_lst) == 0:
             continue
+        if table_seq_no < start_seq_no:
+            continue
+
         out_data = []
         table_id = table_sql_lst[0]['meta']['table_id']
         print(f'No.{table_seq_no} id={table_id}')
-        out_sql_info_file = os.path.join(out_dir, f'sql_info_{table_seq_no}_{table_id}.jsonl')
+        out_sql_info_file = os.path.join(out_dir, f'sql_{table_seq_no}_{table_id}.jsonl')
         f_o_sql = open(out_sql_info_file, 'w')
         for sql_info in table_sql_lst:
             f_o_sql.write(json.dumps(sql_info) + '\n')
@@ -63,7 +87,10 @@ def main():
         out_file = os.path.join(out_dir, f'question_{table_seq_no}_{table_id}.csv')
         df = pd.DataFrame(data=out_data, columns=out_col_names)
         df.to_csv(out_file)
-   
+        
+        state['seq_no'] = table_seq_no
+        state['id'] = table_id
+        write_state(state_file, state)
 
 #col_no start from 1
 def numer_to_letter(col_no):
