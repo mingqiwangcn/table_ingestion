b70d7c4fa8d4ad422b4a70bc2884bee40a5c2ef6
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-12-28 13:33:35 -0600

    compute metric

---
+++ compute_retr_metrics.py
@@ -0,0 +1,41 @@
+import json
+import argparse
+import os
+import numpy as np
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--work_dir', type=str)
+    parser.add_argument('--dataset', type=str)
+    parser.add_argument('--strategy', type=str)
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = get_args()
+    retr_file = os.path.join(args.work_dir, 'data', args.dataset, 
+                             'query', 'test', args.strategy, 
+                             'fusion_retrieved_top.jsonl')
+    with open(retr_file) as f:
+        retr_metric_lst = []
+        for line in f:
+            item = json.loads(line)
+            table_id_lst = item['table_id_lst']
+            metric_info = {}
+            for table_id in table_id_lst:
+                metric_info[table_id] = {'correct':0}
+            ctx_lst = item['ctxs']
+            for ctx_info in ctx_lst:
+                table_id = ctx_info['tag']['table_id']
+                if table_id in metric_info:
+                    metric_info[table_id]['correct'] = 1
+            
+            correct_lst = [metric_info[a]['correct'] for a in metric_info]
+            retr_metric = np.mean(correct_lst)
+            retr_metric_lst.append(retr_metric)
+
+        recall = np.mean(retr_metric_lst) * 100
+    print('recall %.2f' % recall)
+
+if __name__ == '__main__':
+    main()
