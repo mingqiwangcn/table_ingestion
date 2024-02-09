2ef019e82947da831110e60a02b517f65098fda3
Chameleon Cloud User <mingqiwangcn@gmail.com>
2024-02-09 12:42:59 -0600

    perf opt

--- test.py
+++ test.py
@@ -6,9 +6,11 @@ from serial_schema import SchemaSerializer
 from serial_compress import CompressSerializer
 from serial_numeric import NumericSerializer
 from serial_cpr_scm import CprScmSerializer 
+import multiprocessing
 from multiprocessing import Pool as ProcessPool
 import argparse
 import copy
+import glob
 
 def iterate_table(args):
     data_file = get_table_file(args) 
@@ -17,18 +19,32 @@ def iterate_table(args):
             table_data = json.loads(line)
             yield table_data
 
-def get_table_file(args):
-    data_file = '../data/%s/tables/tables.jsonl' % args.dataset
-    return data_file
+def get_table_files(args):
+    file_pattern = '../data/%s/tables/*.jsonl' % args.dataset
+    file_lst = glob.glob(file_pattern)
+    return file_lst
 
-def read_tables(args):
-    data_file = get_table_file(args) 
-    table_lst = []
-    with open(data_file) as f:
-        for line in tqdm(f):
-            table_data = json.loads(line)
-            table_lst.append(table_data)
-    return table_lst
+def chunk_table(table_data, chunk_size):
+    temp_data = {}
+    for key in table_data:
+        if key == 'rows':
+            temp_data[key] = []
+        temp_data[key] = table_data[key]
+
+    row_data = table_data['rows']
+    row_cnt = len(row_data)
+    if row_cnt <= chunk_size:
+        return [table_data]
+
+    table_data_lst = []
+    for offset in range(0, row_cnt, chunk_size):
+        pos = offset + chunk_size
+        batch_row_data = row_data[offset:pos]
+        batch_table_data = copy.deepcopy(temp_data) 
+        temp_data['rows'] = batch_row_data
+        table_data_lst.append(temp_data) 
+
+    return table_data_lst
 
 def init_worker(args):
     global tsl
@@ -47,11 +63,60 @@ def init_worker(args):
         raise ValueError('Strategy (%s) not supported.' % args.strategy)
 
 def process_table(arg_info):
-    table_data = arg_info['table_data']
-    block_lst = []
+    
+    process_info = str(multiprocessing.current_process())
+
+    task_id = arg_info['task_id']
+    args = arg_info['args']
+    table_file = arg_info['table_file']
+    with open(table_file) as f:
+        table_data = json.load(f)
+    
+    row_cnt = len(table_data['rows'])
+
+    table_id = table_data['tableId']
+    f_o_part = None
+    f_o_cpr_part = None
+    f_o_scm_part = None
+    
+    serial_file_name = f'{table_id}_serial.jsonl' 
+    out_part_file = None
+    out_cpr_part_file = None
+    out_scm_part_file = None
+    if args.strategy != 'cpr_scm':
+        out_dir = arg_info['out_dir'] 
+        out_part_file = os.path.join(out_dir, serial_file_name) 
+        #f_o_part = open(out_part_file, 'w')
+    else:
+        out_dir_cpr = arg_info['out_dir_cpr']
+        out_cpr_part_file = os.path.join(out_dir_cpr, serial_file_name)
+        #f_o_cpr_part = open(out_cpr_part_file, 'w')
+
+        out_dir_scm = arg_info['out_dir_scm']
+        out_scm_part_file = os.path.join(out_dir_scm, serial_file_name)
+        #f_o_scm_part = open(out_scm_part_file, 'w')
+
+    serial_block_lst = []
     for serial_block in tsl.serialize(table_data):
-        block_lst.append(serial_block)
-    return block_lst
+        serial_block_lst.append(serial_block)
+   
+    return serial_block_lst
+    #for serial_block in serial_block_lst:
+    #    do_write(args, f_o_part, f_o_cpr_part, f_o_scm_part, serial_block)
+    
+    if f_o_part is not None:
+        f_o_part.close()
+    if f_o_cpr_part is not None:
+        f_o_cpr_part.close()
+    if f_o_scm_part is not None:
+        f_o_scm_part.close()
+    
+    out_part_info = {
+        'out_part_file':out_part_file,
+        'out_cpr_part_file':out_cpr_part_file,
+        'out_scm_part_file':out_scm_part_file
+    }
+    return serial_block_lst
 
 def get_out_file(args, part_name=None):
     folder_name = args.strategy if part_name is None else f'{args.strategy}_{part_name}'
@@ -59,26 +124,34 @@ def get_out_file(args, part_name=None):
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
     out_file = os.path.join(out_dir, 'passages.jsonl')
-    return out_file 
+    return out_dir, out_file 
 
 def main():
     args = get_args()
     block_id = 0
 
+    out_dir = None
+    out_file = None
+    out_dir_cpr = None
+    out_file_cpr = None
+    out_dir_scm = None
+    out_file_scm = None
+
     if args.strategy != 'cpr_scm':
-        out_file = get_out_file(args)
+        out_dir, out_file = get_out_file(args)
         if os.path.exists(out_file):
             answer = input('(%s) already exists. Continue?(y/n)' % out_file)
             if answer != 'y':
                 return
     else:
-        out_file_cpr = get_out_file(args, part_name='compress')
-        out_file_scm = get_out_file(args, part_name='schema')
-        for out_file in [out_file_cpr, out_file_scm]:
-            if os.path.exists(out_file):
-                answer = input('(%s) already exists. Continue?(y/n)' % out_file)
+        out_dir_cpr, out_file_cpr = get_out_file(args, part_name='compress')
+        out_dir_scm, out_file_scm = get_out_file(args, part_name='schema')
+        for data_file in [out_file_cpr, out_file_scm]:
+            if os.path.exists(data_file):
+                answer = input('(%s) already exists. Continue?(y/n)' % data_file)
                 if answer != 'y':
                     return
+    
     f_o = None
     f_o_cpr = None
     f_o_scm = None
@@ -88,36 +161,40 @@ def main():
         f_o_cpr = open(out_file_cpr, 'w')        
         f_o_scm = open(out_file_scm, 'w') 
 
+    table_file_lst = get_table_files(args)
+    arg_info_lst = []
+    for task_id, table_file in enumerate(table_file_lst):
+        arg_info = {
+            'task_id':(task_id + 1),
+            'args':args,
+            'table_file':table_file,
+            'out_dir':out_dir,
+            'out_dir_cpr':out_dir_cpr,
+            'out_dir_scm':out_dir_scm
+        }
+        arg_info_lst.append(arg_info)
+    
     if not args.debug:
-        print('Loading tables')
-        table_lst = read_tables(args)
-        arg_info_lst = []
-        for table_data in table_lst:
-            if len(table_data['rows']) == 0:
-                continue
-            arg_info = {
-                'table_data':table_data,
-            }
-            arg_info_lst.append(arg_info)
-        
-        if len(arg_info_lst) == 0:
-            print('No table data')
-            return
-        work_pool = ProcessPool(initializer=init_worker, initargs=(args, ))
-        for table_block_lst in tqdm(work_pool.imap_unordered(process_table, arg_info_lst), total=len(table_lst)):
-            for serial_block in table_block_lst:
-                serial_block['p_id'] = block_id
-                block_id += 1
-                do_write(args, f_o, f_o_cpr, f_o_scm, serial_block)
+        num_workers = os.cpu_count()
+        work_pool = ProcessPool(num_workers, initializer=init_worker, initargs=(args, ))
+        task_batch_size = 500
+        num_tasks = len(arg_info_lst)
+        for start_pos in range(0, num_tasks, task_batch_size):
+            end_pos = min(start_pos + task_batch_size, num_tasks)
+            batch_arg_lst = arg_info_lst[start_pos:end_pos]
+            batch_task_num = len(batch_arg_lst)
+            for serial_block_lst in tqdm(work_pool.imap_unordered(process_table, batch_arg_lst), total=batch_task_num):
+                for serial_block in serial_block_lst:
+                    serial_block['p_id'] = block_id
+                    block_id += 1
+                    do_write(args, f_o, f_o_cpr, f_o_scm, serial_block)
     else:
         init_worker(args)
-        for table_data in tqdm(iterate_table(args)):
-            arg_info = {'table_data':table_data}
-            table_block_lst = process_table(arg_info)
-            for serial_block in table_block_lst:
-                serial_block['p_id'] = block_id
-                block_id += 1
-                do_write(args, f_o, f_o_cpr, f_o_scm, serial_block)
+        out_part_info_lst = []
+        #import pdb; pdb.set_trace()
+        for arg_info in tqdm(arg_info_lst):
+            out_part_info = process_table(arg_info)
+            out_part_info_lst.append(out_part_info)
     
     if f_o is not None: 
         f_o.close()
