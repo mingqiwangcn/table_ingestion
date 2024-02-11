a05860dc6b620e2ef8c4e0f9575dc5bcb80e0978
Chameleon Cloud User <mingqiwangcn@gmail.com>
2024-02-11 16:20:57 -0600

    remove config

--- encode.py
+++ encode.py
@@ -1,19 +1,18 @@
 import argparse
 import os
 import generate_passage_embeddings as passage_encoder
-from trainer import read_config
 import util
 
 EmbFileTag = '_embeddings'
 
-def get_encoder_args(model_path, config, show_progress=True):
+def get_encoder_args(model_path, show_progress=True):
     encoder_args = argparse.Namespace(is_student=True,
                                       passages=None,
                                       output_path=None,
                                       output_batch_size=500000,
                                       shard_id=0,
                                       num_shards=1,
-                                      per_gpu_batch_size=config['encode_batch_size'],
+                                      per_gpu_batch_size=3000,
                                       passage_maxlength=util.Max_Seq_Length,
                                       model_path=model_path,
                                       no_fp16=False,
@@ -21,11 +20,11 @@ def get_encoder_args(model_path, config, show_progress=True):
                                      )
     return encoder_args
 
-def encode_blocks(work_dir, block_file, config):
+def encode_blocks(work_dir, block_file):
     print('Encoding %s' % block_file)
     encoder_model = os.path.join(work_dir, 'models/student_tqa_retriever_step_29500')
     out_emb_file_lst = []
-    encoder_args = get_encoder_args(encoder_model, config, show_progress=False)
+    encoder_args = get_encoder_args(encoder_model, show_progress=False)
     encoder_args.passages = block_file
     passage_dir = os.path.dirname(block_file)
     base_name = os.path.basename(block_file)
@@ -37,9 +36,8 @@ def encode_blocks(work_dir, block_file, config):
 
 def main():
     args = get_args()
-    config = read_config()
     block_file = f'./output/{args.dataset}/{args.strategy}/passages.jsonl'
-    encode_blocks(args.work_dir, block_file, config)
+    encode_blocks(args.work_dir, block_file)
 
 def get_args():
     parser = argparse.ArgumentParser()
