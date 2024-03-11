f5ba275232b2b34c2777d47810b80163453d4d6a
Chameleon Cloud User <mingqiwangcn@gmail.com>
2024-03-10 22:39:58 -0500

    need to redo schema group

--- code_book.py
+++ code_book.py
@@ -8,15 +8,15 @@ class CodeBook:
     # Let x is the size of cell text, y is the # of occurences of the cell text. Since the compressed text inlcude [E?] y times and also the text "[E?] is {cell text} [SEP]", 
     # we must have yx > y + x + 3, so x > (y+3) / (y-1) = 1 + ( 4 / (y-1) ), which is the condition to start compression. We must tract those cells before the condition is stisfied, so that
     # we can update them with codes
-    def get_code(self, cell_info):
-        text = cell_info['text']
+    def get_code(self, cell_info, text_key='text', size_key='size'):
+        text = cell_info[text_key]
         key = util.get_hash_key(text)
         if key not in self.text_dict:
             self.text_dict[key] = {'count':0, 'pre_cells':[]}
         text_info = self.text_dict[key]
         text_info['count'] += 1
         
-        x = cell_info['size']
+        x = cell_info[size_key]
         y = text_info['count']
 
         if key in self.code_dict:
@@ -31,14 +31,14 @@ class CodeBook:
             self.code_dict[key] = {'code':code}
             compress_code = code + ' is ' + text + ' ' + self.tokenizer.sep_token + ' ' 
             cell_info['compress_code'] = compress_code
-            cell_info['cpr_code_size'] = 1 + 1 + cell_info['size'] + 1
+            cell_info['cpr_code_size'] = 1 + 1 + cell_info[size_key] + 1
             cell_info['pre_cells'] = text_info['pre_cells']
 
             return code, 1
         else:
             pre_cell_lst = text_info['pre_cells']
             pre_cell_lst.append(cell_info)
-            return text, cell_info['size']  
+            return text, cell_info[size_key]  
 
     def reset(self):
         self.text_dict = {}
--- serial_agree_coding.py
+++ serial_agree_coding.py
@@ -14,6 +14,87 @@ class AgreeCodingSerializer(TableSerializer):
         print('start compute agr set')
         agr_dict = self.compute_agree_set(table_data)
         print('agr set ok')
+        
+        col_group_dict = {}
+        pq_agr_key_lst = self.create_agr_priority_queue(agr_dict)
+        for _ in range(len(pq_agr_key_lst)):
+            _, agr_key = heapq.heappop(pq_agr_key_lst)
+            agr_item = agr_dict[agr_key]
+            agr_class_lst = agr_item['agr_class_lst'] # already sorted by col index
+            agr_col_group = self.agr_class_to_col_group(agr_class_lst)
+            tuple_key = tuple(agr_col_group)
+            if tuple_key not in col_group_dict:
+                block_lst = self.split_columns(agr_col_group, table_data)
+                col_group_dict[tuple_key] = {'schema_blocks':block_lst}
+            
+            schema_block_lst = col_group_dict[tuple_key]['schema_blocks']
+            for schema_block in schema_block_lst:
+                row_lst = list(agr_item['row_set'])
+                yield from self.serialize_schema_block(table_data, schema_block, agr_item) 
+
+    def serialize_schema_block(table_data, schema_block, row_lst):
+        schema_text = self.get_window_schema_text(table_data, schema_block)
+        self.serial_window.set_schema_text(schema_text)
+        yield from self.get_wnd_block(table_data, schema_block, row_lst)
+   
+    def get_serial_text(self, table_data, row, col_group, last_in_block):
+        col_data = table_data['columns']
+        first_col = col_group[0]
+        col_info = col_data[first_col]
+        sep_token = ';' if not last_in_block else self.tokenizer.sep_token
+        
+        row_cells = table_data['rows'][row]['cells']
+        cell_info = row_cells[first_col]
+        text_key = 'text'
+        size_key = 'size'
+        if len(col_group) > 1:
+            text_key = 'group_text'
+            size_key = 'group_size'
+            cell_info[text_key] = ' , '.join([row_cells[col]['text'] for col in col_group])
+            cell_info[size_key] = sum([row_cells[col]['size'] for col in col_group]) + len(col_group) - 1   
+
+        cell_text, cell_size = self.get_cell_text(cell_info, text_key, size_key)
+
+        serial_text = cell_text + ' ' + sep_token + ' '
+        serial_size = cell_size + 1 
+
+        self.update_related_cell(cell_info, serial_text, serial_size)
+
+        return serial_text, serial_size
+
+    def get_cell_text(self, cell_info, text_key, size_key):
+        text = cell_info[text_key]
+        if cell_info[size_key] < 2:
+            return text, cell_info[size_key]
+
+        code, code_size = self.serial_window.cell_code_book.get_code(cell_info, text_key=text_key, size_key=size_key)
+        return code, code_size
+
+    def update_related_cell(self, cell_info, serial_text, serial_size):
+        compress_code = cell_info.get('compress_code', None)
+        if compress_code is not None:
+            pre_cell_lst = cell_info['pre_cells']
+            for pre_cell in pre_cell_lst:
+                pre_cell['updated_serial_text'] = serial_text
+                pre_cell['updated_serial_size'] =serial_size
+
+    def get_wnd_block(self, table_data, schema_block, row_lst):
+        block_groups = schema_block['col_groups']
+        for row in row_lst:
+            for offset, col_group in enumerate(block_groups):
+                last_in_block = (offset == len(block_groups))
+                serial_text, serial_size = self.get_serial_text(table_data, row, col_group, last_in_block)
+                first_col = col_group[0]
+                if self.serial_window.can_add(table_data, row, first_col, serial_text, serial_size):
+                    self.serial_window.add(table_data, row, first_col)
+                else:
+                    serial_block = self.serial_window.pop(table_data)
+                    yield serial_block
+                    self.serial_window.add(table_data, row, first_col)
+
+        if self.serial_window.can_pop():
+            serial_block = self.serial_window.pop(table_data)
+            yield serial_block
 
     def create_agr_priority_queue(self, agr_dict):
         pq_item_lst = []
@@ -31,20 +112,57 @@ class AgreeCodingSerializer(TableSerializer):
             col = int(agr_class.split('-')[0])
             col_group.append(col)
         return col_group
-    
-    def split_columns(self, agr_col_group, table_data):
-        schema_lst = []
-        col_data = table_data['columns']
-         
 
-    def get_schema_groups(self, pq_agr_key_lst, agr_dict, table_data):
-        col_group_dict = {}
-        for _ in range(len(pq_agr_key_lst)):
-            _, agr_key = heapq.heappop(pq_agr_key_lst)
-            agr_item = agr_dict[agr_key]
-            agr_class_lst = agr_item['agr_class_lst'] # already sorted by col index
-            agr_col_group = self.agr_class_to_col_group(agr_class_lst)
-             
+    def get_col_groups(self, agr_col_group, table_data):
+        agr_col_set = set(agr_col_group)
+        col_data = table_data['columns']
+        col_group_lst = []
+        for col, _ in enumerate(col_data):
+            if col in agr_col_set:
+                if col == agr_col_group[0]:
+                    col_group = agr_col_group
+            else:
+                col_group = [col]
+            col_group_lst.append(col_group)
+        return col_group_lst
+   
+    # Use a single partition for all to share schema. 
+    # Try to put agree groups in the same schema group
+    def split_columns(self, agr_dict, table_data):
+        block_lst = []
+        block_groups = []
+        block_size = 0
+        col_data = table_data['columns']
+        col_group_lst = self.get_col_groups(agr_col_group, table_data)
+        max_size = int(self.serial_window.wnd_size * util.Max_Header_Meta_Ratio)
+        for col_group in col_group_lst:
+            group_size = sum([col_data[col]['size'] for col in col_group]) + len(col_group) - 1 # use '&' to concatenate 
+            serial_text = ' & '.join(col_data[col]['text'] for col in col_group) + ';'
+            serial_size = group_size + 1 # a sep token for each col group 
+            if block_size + serial_size <= max_size:
+                block_groups.append(col_group)
+                block_text += serial_text
+                block_size += serial_size
+            else:
+                block_info = self.get_block_info(block_cols, block_text, block_size)
+                block_lst.append(block_info)
+                block_groups = [col_group]
+                block_text = serial_text
+                block_size = serial_size
+        if block_size > 0:
+            block_info = self.get_block_info(block_cols, block_text, block_size)
+            block_lst.append(block_info)
+        return block_lst
+
+    def get_block_info(self, block_groups, block_text, block_size):
+        assert block_text[-1] == ';'
+        updated_block_text = block_text[:-1] + self.tokenizer.sep_token
+        block_info = {
+            'col_groups':block_cols,
+            'text':updated_block_text,
+            'size':block_size
+        }
+        return block_info
 
     def check_agr_key(self, agr_dict):
         key_lst = list(agr_dict.keys())
