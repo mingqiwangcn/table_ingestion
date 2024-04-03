1c28fba69bbe784b15b5ff35fa5206d83c6b3fe6
Chameleon Cloud User <mingqiwangcn@gmail.com>
2024-04-02 22:18:18 -0500

    output top retr

--- compute_retr_metrics.py
+++ compute_retr_metrics.py
@@ -14,42 +14,47 @@ def get_args():
 
 def main():
     args = get_args()
-    retr_file = os.path.join(args.work_dir, 'data', args.dataset, 
-                             'query', 'test', args.strategy, 
-                             'exact/fusion_retrieved.jsonl')
-    
+    out_retr_dir = os.path.join(args.work_dir, 'data', args.dataset, 
+                            'query', 'test', args.strategy, 
+                            'exact')
+    retr_file = os.path.join(out_retr_dir, 'fusion_retrieved.jsonl')
+    out_top_retr_file = os.path.join(out_retr_dir, f'retr_top_{args.top}_table.jsonl')
+    f_o = open(out_top_retr_file, 'w')
     with open(retr_file) as f:
         retr_metric_lst = []
         for line in f:
             item = json.loads(line)
-            table_id_lst = item['table_id_lst']
-            metric_info = {}
-            for table_id in table_id_lst:
-                metric_info[table_id] = {'correct':0}
-            ctx_lst = get_top_tables(item['ctxs'], args.top)
-            for ctx_info in ctx_lst:
-                table_id = ctx_info['tag']['table_id']
-                if table_id in metric_info:
-                    metric_info[table_id]['correct'] = 1
-            
-            correct_lst = [metric_info[a]['correct'] for a in metric_info]
-            retr_metric = np.mean(correct_lst)
+            table_answer_set = set(item['table_id_lst'])
+            top_table_lst, top_table_dict = get_top_tables(item['ctxs'], args.top, table_answer_set)
+            retr_metric = max([top_table_dict[a]['correct'] for a in top_table_lst])
             retr_metric_lst.append(retr_metric)
+            out_item = {
+                'id':item['id'],
+                'question':item['question'], 
+                'table_answers':item['table_id_lst'],
+                'top_tables':top_table_lst,
+                'table_data':top_table_dict
+            }
+            f_o.write(json.dumps(out_item) + '\n')
+        precision = np.mean(retr_metric_lst) * 100
+    f_o.close()
+    print('%s precision@%d %.2f' % (args.strategy, args.top, precision))
+    print(f'output retr is {out_top_retr_file}')
 
-        recall = np.mean(retr_metric_lst) * 100
-    print('%s recall@%d %.2f' % (args.strategy, args.top, recall))
-
-def get_top_tables(ctx_lst, num_top):
-    retr_ctx_lst = []
-    table_set = set()
+def get_top_tables(ctx_lst, num_top, table_answer_set):
+    table_lst = []
+    table_dict = {}
     for ctx_info in ctx_lst:
         table_id = ctx_info['tag']['table_id']
-        if table_id not in table_set:
-            if len(table_set) == num_top:
+        if table_id not in table_dict:
+            if len(table_dict) == num_top:
                 break
-            table_set.add(table_id)
-        retr_ctx_lst.append(ctx_info)
-    return retr_ctx_lst
+            table_lst.append(table_id)
+            correct = int(table_id in table_answer_set)
+            table_dict[table_id] = {'correct':correct, 'ctxs':[]}
+        table_ctx_lst = table_dict[table_id]['ctxs']
+        table_ctx_lst.append(ctx_info)
+    return table_lst, table_dict
 
 
 if __name__ == '__main__':
