cc2d23f44d7b28c1d86d5debfaca233ef2774bab
Chameleon Cloud User <mingqiwangcn@gmail.com>
2024-04-14 00:35:53 -0500

    merge code info

--- code_book.py
+++ code_book.py
@@ -13,16 +13,16 @@ class CodeBook:
     def get_code(self, cell_info, text_key='text', size_key='size'):
         text = cell_info[text_key]
         key = util.get_hash_key(text)
-        if key not in self.text_dict:
-            self.text_dict[key] = {'count':0, 'pre_cells':[]}
-        text_info = self.text_dict[key]
-        text_info['count'] += 1
+        if key not in self.code_dict:
+            self.code_dict[key] = {'count':0, 'code':None, 'pre_cells':[]}
+        code_info = self.code_dict[key]
+        code_info['count'] += 1
         
         x = cell_info[size_key]
-        y = text_info['count']
+        y = code_info['count']
 
-        if key in self.code_dict:
-            code = self.code_dict[key]['code']
+        code = self.code_dict[key]['code']
+        if code is not None:
             return code, 1
 
         if y > 1 and ( x > 1 + ( 4 / (y - 1) ) ): 
@@ -30,22 +30,20 @@ class CodeBook:
             if code not in self.special_token_dict:
                 self.special_token_dict[code] = True
             self.tokenizer.add_tokens([code], special_tokens=True)
-            self.code_dict[key] = {'code':code}
+            self.code_dict[key]['code'] = code
             compress_code = code + ' is ' + text + ' ' + self.tokenizer.sep_token + ' ' 
             cell_info['compress_code'] = compress_code
             cell_info['cpr_code_size'] = 1 + 1 + cell_info[size_key] + 1
             cell_info['pre_cells'] = text_info['pre_cells']
-
             return code, 1
         else:
-            pre_cell_lst = text_info['pre_cells']
+            pre_cell_lst = code_info['pre_cells']
             pre_cell_lst.append(cell_info)
             return text, cell_info[size_key]  
 
     def reset(self):
-        self.text_dict = {}
-        self.special_token_dict = {}
         self.code_dict = {}
         self.code_text = ''
         self.code_size = 0
-
+        self.special_token_dict = {}
+        
