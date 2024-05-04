f2711e8fa3acc2382b40aeb07ef74069108d6acb
Chameleon Cloud User <mingqiwangcn@gmail.com>
2024-05-04 09:00:59 -0500

    train data in work

--- encoder_tune/create_train_data.py
+++ encoder_tune/create_train_data.py
@@ -29,6 +29,32 @@ def read_questions(args):
             q_info_lst.append(q_info)
     return q_info_lst
 
+def read_cpr_passages(args):
+    p_tab_no_map = {}
+    p_tab_id_map = {}
+    cpr_passage_file = os.path.join(args.sample_dir, 'sample_cpr_passages.jsonl')
+    with open(cpr_passage_file) as f:
+        for line in tqdm(f):
+            p_info = json.loads(line)
+            table_number = p_info['table_number']
+            table_id = p_info['table_id']
+            p_tab_no_map[table_number] = p_info
+            if table_id not in p_tab_id_map:
+                p_tab_id_map[table_id] = []
+            sub_cpr_lst = p_tab_id_map[table_id]
+            sub_cpr_lst.append(p_info)
+    return p_tab_no_map, p_tab_id_map
+
+def read_base_passages(args):
+    p_dict = {}
+    base_passage_file = os.path.join(args.sample_dir, 'sample_base_passages.jsonl')
+    with open(base_passage_file) as f:
+        for line in tqdm(f):
+            p_info = json.loads(line)
+            p_id = p_info['p_id']
+            p_dict[p_id] = p_info
+    return p_dict
+
 def main():
     args = get_args()
     work_dir = os.path.dirname(os.getcwd())
@@ -36,22 +62,32 @@ def main():
     args.sample_dir = os.path.join(work_dir, 'output', args.dataset, args.strategy, 'samples')
     table_dict, table_number_map = read_tables(args)
     q_info_lst = read_questions(args)
+    cpr_tab_no_map, cpr_tab_id_map = read_cpr_passages(args)
+    base_map = read_base_passages(args)
     example_lst = []
     for q_id, q_info in enumerate(q_info_lst):
         q_table_number = q_info['table_number']
-        example = {
+        cpr_example = {
             'id':q_id,
             'q_id':q_id,
             'question':q_info['question'],
-            'target':'N/A',
-            'ctx':[]
+            'answers':[]
+            'target':'',
+            'ctx':[],
+            'pos_idxes':[],
+            'neg_idxes':[]
         }
-        pos_ctx = {
-            'title':
+        cpr_pos_passage = cpr_tab_no_map[q_table_number]['passage']
+        cpr_pos_ctx = {
+            'title':'',
+            'text':pos_passage,
+            'score':1.0
         }
-        ctx_lst = example['ctx']
+        cpr_neg_passage_lst = []
+        cpr_ctx_lst = example['ctx']
 
-
+def get_cpr_neg_passage_lst():
+    return
 
 def get_args():
     parser = argparse.ArgumentParser()
