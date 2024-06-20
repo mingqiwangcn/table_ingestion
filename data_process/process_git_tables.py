68eed587dc708c8ba909b951108a5b38da828830
Chameleon Cloud User <mingqiwangcn@gmail.com>
2024-06-19 22:39:19 -0500

    keep english tables only

--- data_process/process_git_tables.py
+++ data_process/process_git_tables.py
@@ -1,10 +1,18 @@
 import pandas as pd
-from multiprocessing import Pool as ProcessPool
 import os
 import glob
 import random
 from pathlib import Path
 from tqdm import tqdm
+
+
+def isEnglish(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
 
 def main():
     home_dir = os.path.expanduser("~")
@@ -17,16 +25,24 @@ def main():
         return
     file_pattern = os.path.join(home_dir, 'downloads', 'git_tables', '*/*.parquet')
     file_lst = glob.glob(file_pattern)
-    sample_files = random.sample(file_lst, 10000)
+    sample_files = random.sample(file_lst, 20000)
     for par_file in tqdm(sample_files):
         folder_name = Path(os.path.dirname(par_file)).stem
-        df = pd.read_parquet(par_file)
+        try:
+            df = pd.read_parquet(par_file)
+        except:
+            continue
+        
+        col_text = '  '.join(df.columns.tolist())
+        if not isEnglish(col_text):
+            continue
+
         file_name = Path(par_file).stem
         out_folder = os.path.join(out_dir, folder_name)
         if not os.path.isdir(out_folder):
             os.makedirs(out_folder)
         out_file = os.path.join(out_folder, file_name + '.csv')
-        df.to_csv(out_file)
+        df.to_csv(out_file, index=False)
 
     print('output to ' + out_dir)
 
